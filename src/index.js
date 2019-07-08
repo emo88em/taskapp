659b8cae241983da2ec3bde46d38c7d77d0f9f84
Eric <user@Users-MBP.attlocal.net>
2019-07-08 14:49:23 -0500

    added middleware and auth to endpoints

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ app.listen(port, () => {
 	console.log('Server is up and running on ' + port);
 });
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-const myFunction = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse')
-    console.log(token)
+// const myFunction = async () => {
+//     const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse')
+//     console.log(token)
 
-    const data = jwt.verify(token, 'thisismynewcourse', { expiresIn: '10 seconds'})
-    console.log(data)
-}
+//     const data = jwt.verify(token, 'thisismynewcourse', { expiresIn: '7 days'})
+//     console.log(data)
+// }
 
-myFunction()
+// myFunction()
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -39,8 +40,26 @@ const userSchema = new mongoose.Schema({
 				throw new Error('Age must be a positive number');
 			}
 		}
-	}
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				required: true
+			}
+		}
+	]
 });
+
+//look at this again
+userSchema.methods.generateAuthToken = async function() {
+	const user = this;
+	const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+	return token;
+};
 
 //creating findByCredentials login endpoint
 userSchema.statics.findByCredentials = async (email, password) => {
